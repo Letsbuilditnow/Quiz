d52da7c7ebba06ef87d2fd049ce4cd66142ada50
mmmmm1233212344 <abhaymilan8@gmail.com>
2023-01-25 10:13:01 +0530

    Home Page Section Basic Desing Done

---
+++ react-quiz/src/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <main>
+    <section className='start-section'>
+     <div>
+      <h1>Quizzical</h1>
+      <span>Challenge yourself and have fun while learning with our quiz game</span>
+      <br />
+      <button>Start Game</button>
+     </div>
+    </section>
+    </main>
+  )
+}
+
+export default Home
