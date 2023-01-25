47dd4acee82e33cb75e1824bbb080812c8567fa0
mmmmm1233212344 <abhaymilan8@gmail.com>
2023-01-25 20:27:41 +0530

    Dynamically rendered the questions

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -3,10 +3,18 @@ import "./App.css"
 import Home from './Home'
 import Questions from './Questions'
 export default function App() {
+const [apiData,setApiData]=React.useState([])
+
+React.useEffect(()=>{
+
+  fetch("https://opentdb.com/api.php?amount=4")
+  .then(response =>response.json())
+  .then(data=>setApiData(data.results))
+},[])
   let arr=[1,2,3,4]
   let questionsElements=[]
-  questionsElements = arr.map(a=>{
-   return <Questions/>
+  questionsElements = apiData.map(a=>{
+   return <Questions question={a.question}/>
    
   }
   ) 
--- react-quiz/src/Questions.jsx
+++ react-quiz/src/Questions.jsx
@@ -1,7 +1,7 @@
 import Buttons from "./Buttons";
 import React from "react";
 
-function Questions() {
+function Questions(props) {
  let arr=[1,2,3,4]
   let buttonElements=arr.map(a=><Buttons/>)
   return (
@@ -9,7 +9,7 @@ function Questions() {
      
             <div className="section-questions">
               <div className="section-question">
-                <h4>choose a number</h4>
+                <h4>{props.question}</h4>
               </div>
               <div className="section-buttons">
               {buttonElements}
